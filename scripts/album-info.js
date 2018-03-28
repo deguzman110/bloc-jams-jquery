3f219fb39722e6bc1f45dbb5c2e03903f8ab5e18
Anthony DeGuzman <deguzman110@gmail.com>
2018-03-28 14:05:11 -0400

    Assignment 08 Merge album-info.js

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,9 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('.artist').text(album.artist);
+  $('#release-info').text(album.releaseInfo);
 }
+
+// ('.artist') used as "artist" in within a class in index.html
+// ('#release-info') used as "releaseInfo" is within an id
